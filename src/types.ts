14ce96b11f2fe5f5fb74760d64ae5abd6c351fdf
thundering-silence <25010047+thundering-silence@users.noreply.github.com>
2025-04-08 18:12:36 +0200

    Quote notification type

--- src/types.ts
+++ src/types.ts
@@ -26,6 +26,14 @@ export type Quote = {
   validUntil: number;
 };
 
+export type QuoteNotification = {
+  rfqId: string;
+  assetAddress: string;
+  chainId: number;
+  newBest: string;
+  yours: string;
+};
+
 export type Transfer = {
   amout: string;
   asset: string;
@@ -46,60 +54,75 @@ export type JSONResponseHandler = (res: JSONRPCResponse) => void;
 // Type predicate for Request
 export function isRequest(obj: any): obj is Request {
   return (
-    typeof obj === 'object' &&
+    typeof obj === "object" &&
     obj !== null &&
-    typeof obj.asset === 'string' &&
-    typeof obj.assetName === 'string' &&
-    typeof obj.chainId === 'number' &&
-    typeof obj.expiry === 'number' &&
-    typeof obj.isPut === 'boolean' &&
-    typeof obj.quantity === 'string' &&
-    typeof obj.strike === 'string' &&
-    typeof obj.taker === 'string'
+    typeof obj.asset === "string" &&
+    typeof obj.assetName === "string" &&
+    typeof obj.chainId === "number" &&
+    typeof obj.expiry === "number" &&
+    typeof obj.isPut === "boolean" &&
+    typeof obj.quantity === "string" &&
+    typeof obj.strike === "string" &&
+    typeof obj.taker === "string"
   );
 }
 
 // Type predicate for Quote
 export function isQuote(obj: any): obj is Quote {
   return (
-    typeof obj === 'object' &&
+    typeof obj === "object" &&
     obj !== null &&
-    typeof obj.assetAddress === 'string' &&
-    typeof obj.chainId === 'number' &&
-    typeof obj.expiry === 'number' &&
-    typeof obj.isPut === 'boolean' &&
-    typeof obj.isTakerBuy === 'boolean' &&
-    typeof obj.maker === 'string' &&
-    typeof obj.nonce === 'string' &&
-    typeof obj.price === 'string' &&
-    typeof obj.quantity === 'string' &&
-    typeof obj.signature === 'string' &&
-    typeof obj.strike === 'string' &&
-    typeof obj.validUntil === 'number'
+    typeof obj.assetAddress === "string" &&
+    typeof obj.chainId === "number" &&
+    typeof obj.expiry === "number" &&
+    typeof obj.isPut === "boolean" &&
+    typeof obj.isTakerBuy === "boolean" &&
+    typeof obj.maker === "string" &&
+    typeof obj.nonce === "string" &&
+    typeof obj.price === "string" &&
+    typeof obj.quantity === "string" &&
+    typeof obj.signature === "string" &&
+    typeof obj.strike === "string" &&
+    typeof obj.validUntil === "number"
   );
 }
 
 // Type predicate for Transfer
 export function isTransfer(obj: any): obj is Transfer {
   return (
-    typeof obj === 'object' &&
+    typeof obj === "object" &&
     obj !== null &&
-    typeof obj.amout === 'string' &&
-    typeof obj.asset === 'string' &&
-    typeof obj.chain_id === 'number' &&
-    typeof obj.is_deposit === 'boolean' &&
-    typeof obj.nonce === 'string'
+    typeof obj.amout === "string" &&
+    typeof obj.asset === "string" &&
+    typeof obj.chain_id === "number" &&
+    typeof obj.is_deposit === "boolean" &&
+    typeof obj.nonce === "string"
   );
 }
 
 // Type predicate for JSONRPCResponse
 export function isJSONRPCResponse(obj: any): obj is JSONRPCResponse {
   return (
-    typeof obj === 'object' &&
+    typeof obj === "object" &&
     obj !== null &&
-    typeof obj.jsonrpc === 'string' &&
-    typeof obj.id === 'string' &&
-    typeof obj.method === 'string' &&
-    (typeof obj.params === 'object' || Array.isArray(obj.params) || typeof obj.params === 'string')
+    typeof obj.jsonrpc === "string" &&
+    typeof obj.id === "string" &&
+    typeof obj.method === "string" &&
+    (typeof obj.params === "object" ||
+      Array.isArray(obj.params) ||
+      typeof obj.params === "string")
+  );
+}
+
+// Type predicate for QuoteNotification
+export function isQuoteNotification(obj: any): obj is QuoteNotification {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    typeof obj.rfqId === "string" &&
+    typeof obj.assetAddress === "string" &&
+    typeof obj.chainId === "number" &&
+    typeof obj.newBest === "string" &&
+    typeof obj.yours === "string"
   );
 }
